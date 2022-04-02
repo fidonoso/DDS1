5c2eba6376780b3b58549b79435d813c141071a5
Fernando Donoso <fidonoso@gmail.com>
2022-04-01 21:05:57 -0300

    clase 01-04-2022

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
-
-//Rutas
-app.get('/hola',(request , response)=>{
-    response.send('Hello f****g wolrd!!!')
-})
-
 app.listen(3000,() =>{
     console.log('Escuchando en el puerto 3000')
 });
 
-app.get('/getName',(request , response)=>{
+//Rutas
+app.get('/hola',(request , response)=>{
+    response.send(`<h1> quien eres? </h1>
+                    <form method='GET' action='/responder'>
+                        <label>Escribe tu nombre</label>
+                        <input type='text' name='nombre' ></input>
+                        <button type='submit' >enviar</button>
+                    </form>`
+                        
+                        )
+});
+
+ 
+
+
+app.post('/getName',(request , response)=>{
     response.json({
         nombre: "Antonio Salinas",
         direccion: "Mi Casa 3344",
